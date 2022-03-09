1b2a546ef5aef68f03ffa3ff9d50d87c78fc3818
Kethly <Kethlyfast@gmail.com>
2022-03-09 18:28:22 -0500

    test2

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ $db_name = $_ENV["db_name"];
 $username = $_ENV["username"];
 $password = $_ENV["password"];
 // Create connection
-$conn = new mysqli($host, $username, $password, $db_name);
-
+$conn = mysqli_init();
+$actual_conn = mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($actual_conn->connect_error) {
+  die("Connection failed: " . $actual_conn->connect_error);
   echo("Connection Failed!");
 }
 echo "Connected successfully";
